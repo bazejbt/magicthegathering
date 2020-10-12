4f3d5f71b5adeabd034ef9066d401f6d4a98fcba
bazejbt <bazejbt@yahoo.co.uk>
2020-10-12 13:25:53 +0200

    auth fixed

--- scripts.js
+++ scripts.js
@@ -1,15 +1,28 @@
+const nameInput = document.querySelector("#nameInput")
+const submitBtn = document.querySelector("#subBtn");
 
 
 const getName = () => {
-    const myName = document.querySelector("#nameInput").value;
+    const myName = nameInput.value;
     localStorage.setItem("username", myName);
 }
 
-const submitBtn = document.querySelector("#subBtn");
 submitBtn.addEventListener("click", function(e){
-   // e.preventDefault();
     getName();
+    window.location.href = './home.html';
 });
 
-
-
+nameInput.addEventListener("keyup", function(e){
+    const input = e.target.value;
+    let firstCharCapped = input.substring(0, 1).toUpperCase();
+    let firstChar = input.substring(0, 1);
+    if (input.length >= 3) {
+        if (firstCharCapped === firstChar){
+            submitBtn.disabled = false;
+        } else {
+            submitBtn.disabled = true;
+        }
+    } else {
+        submitBtn.disabled = true;
+    }
+})
